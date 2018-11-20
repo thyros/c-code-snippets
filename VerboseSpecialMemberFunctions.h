96e39b6fe63e294c697e6ea9bb07ae2beedf8882
thyros <thyros@gmail.com>
2018-11-20 08:25:42 +0000

    Update VerboseSpecialMemberFunctions.h

--- VerboseSpecialMemberFunctions.h
+++ VerboseSpecialMemberFunctions.h
@@ -6,6 +6,7 @@ public:
     X(X&& x) : i(++count) { std::cout << "X(&&): " << i << " from: " << x.i << std::endl; }
     X& operator=(const X& x)  { std::cout << "operator=(const&): " << i << " assigned from: " << x.i << std::endl; return *this; }
     X& operator=(X&& x)  { std::cout << "operator=(&&): " << i << " assigned from: " << x.i << std::endl; return *this; }
+    ~X() { std:: cout << "~X(): " << i << std::endl; }
     
     int i = 0;
     static int count;
